d176ececee488be6e4c650110a39ecad141ae913
Bassam-Adnan <b7bassam94@gmail.com>
2023-07-08 09:24:52 +0300

    Final

---
+++ Restaurant-react-main/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Testimonials from "./Testimonials";
+import About from './About';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Testimonials />
+      <About />
+    </>
+  );
+};
+
+export default Home;
